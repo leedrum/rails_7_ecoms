8e3c5aa2e88d6320d1eed1dc4c5b9961213e1e8e
tunglv <tunglv@chartdesk.pro>
2024-01-30 15:04:10 +0700

    fix lint

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,11 @@
+# typed: false
+# frozen_string_literal: true
+
+# Path: app/models/category.rb
 class Category < ApplicationRecord
   has_ancestry
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["ancestry", "created_at", "description", "id", "id_value", "name", "updated_at"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[ancestry created_at description id id_value name updated_at]
   end
 end
