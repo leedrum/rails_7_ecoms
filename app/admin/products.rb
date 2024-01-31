dfd1db0b3b910db8beed920ab2b0e0fad089b83f
tunglv <developer.levantung@gmail.com>
2024-01-31 18:06:40 +0700

    support multi attachment for product

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -19,11 +19,53 @@ ActiveAdmin.register Product do
     actions
   end
 
-  permit_params :name, :description, :price, :quantity, :category_id
+  permit_params :name, :description, :price, :quantity, :category_id, images: []
 
   filter :name, as: :string
   filter :description, as: :string
   filter :price
   filter :quantity
   filter :category, as: :select, collection: proc { Category.all.map { |c| [c.name, c.id] } }
+
+  form html: { multipart: true } do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :quantity
+      li do
+        f.label :category_id
+        collection_select = Category.all.map { |c| [c.name, c.id] }
+        f.select :category_id, collection_select, include_blank: true, selected: f.object.category_id
+      end
+      f.object.images.each do |image|
+        f.hidden_field :images, multiple: true, value: image.signed_id
+        image_tag url_for(image)
+      end
+      f.input :images, as: :file, input_html: { multiple: true }
+    end
+
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :price
+      row :quantity
+      row :category do |product|
+        link_to product.category.name, admin_category_path(product.category)
+      end
+      row :images do
+        div do
+          product.images.each do |img|
+            div do
+              image_tag url_for(img), size: '150x150'
+            end
+          end
+        end
+      end
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@
 
 class Product < ApplicationRecord
   belongs_to :category
+  has_many_attached :images
 
   acts_as_paranoid
 
